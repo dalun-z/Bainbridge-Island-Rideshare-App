4f029ca340dcb342aab4741656319d5a6e40ac21
dalun-z <dzhang1238@gmail.com>
2022-04-07 15:12:54 -0700

    test

--- BICrideshare/App.js
+++ BICrideshare/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Test! Please work!</Text>
+      <Text>Test! Please this works too!</Text>
       <StatusBar style="auto" />
     </View>
   );
